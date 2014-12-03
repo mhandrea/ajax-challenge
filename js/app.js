d3f12e2790d03b8da376f26aab6114e481e51922
Michael Andrea <mhandrea@uw.edu>
2014-12-02 18:07:31 -0800

    almost

--- js/app.js
+++ js/app.js
@@ -4,26 +4,22 @@
     define your module and controllers here
 */
 
-
-// app Lpi7BkWiEtAHRjlc3rofafI3C0UGsCDgf1WWeCjX
-// rest X1QNW3oDTkZeQDf9jt9ZPEUIZ1XdEO34XDYTcItx
-
-angular.module('review_app', ['ui.bootstrap'])
+angular.module('CommentApp', ['ui.bootstrap'])
 	.config(function($httpProvider) {
 		// set default HTTP headers that Parse requires
 		// HTTP request we make
 		$httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'Lpi7BkWiEtAHRjlc3rofafI3C0UGsCDgf1WWeCjX';
-		$httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'X1QNW3oDTkZeQDf9jt9ZPEUIZ1XdEO34XDYTcItx';
+		$httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = '7lgnhwHh6uzz7sojGg8MvPQzSOFoSrihVScosKgC';
 	})
-	.controller('TasksController', function($scope, $http) {
+	.controller('CommentController', function($scope, $http) {
 		var base_url = 'https://api.parse.com/1/classes/comments';
 
-		$scope.get_commments = function() {	
+		$scope.get_comments = function() {	
 			// get all tasks
 			$scope.loading = true;
 			$http.get(base_url) // '?where={"done": false}' 
 				.success(function(response_data) {
-					$scope.tasks = response_data.results
+					$scope.comments = response_data;
 				})
 				.error(function(err) {
 					console.log(err);
@@ -32,7 +28,7 @@ angular.module('review_app', ['ui.bootstrap'])
 					$scope.loading = false;
 				});
 		};
-		$scope.get_commments(); // get all new comments
+		$scope.get_comments(); // get all new comments
 
 		// initialize a new comment object on the same scope as the form
 		$scope.new_comment = {
@@ -42,9 +38,9 @@ angular.module('review_app', ['ui.bootstrap'])
 		// add a new comment to the list
 		$scope.add_comment = function(comment) {
 			$scope.inserting = true;
-			$http.post(comment_url, comment)
+			$http.post(base_url, comment)
 				.success(function(response_data) {
-					comment.object_id = response_data.object_id; // copy reponse data to the comment we just added.
+					comment.objectId = response_data.objectId; // copy reponse data to the comment we just added.
 					$scope.comments.push(comment); // add comment to the list
 					$scope.new_comment = {
 						score: 0
@@ -56,11 +52,11 @@ angular.module('review_app', ['ui.bootstrap'])
 				.finally(function() {
 					$scope.inserting = false;
 				});
-			$scope.get_commments();
+			$scope.get_comments;
 		};
 
 		// function to update an existing comment
-		$scope.upvote_comment = function(commment, pnts_change) {
+		$scope.upvote_comment = function(comment, pnts_change) {
 			$scope.updating = true;
 				var comment = {
 					score: {
@@ -69,7 +65,7 @@ angular.module('review_app', ['ui.bootstrap'])
 					}
 				};
 			};
-			$http.put(comment_url + '/' + comment.object_id, comment)
+			$http.put(base_url + '/' + comment.objectId, comment)
 				.success(function(response_data) {
 					comment.score = response_data.score;
 				})
@@ -79,24 +75,5 @@ angular.module('review_app', ['ui.bootstrap'])
 				.finally(function() {
 					$scope.updating = false;
 				});
-		};
-
+		
 	});
-
-/* .sort(function(x, y) {
-						if (x.score > y.score) {
-							return -1;
-						}
-						if (x.score == y.score) {
-							return 0;
-						}
-						return 1;
-					});&.sort(function(x, y) {
-						if (x.score > y.score) {
-							return -1;
-						}
-						if (x.score == y.score) {
-							return 0;
-						}
-						return 1;
-					});*/
